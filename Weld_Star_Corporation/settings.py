425e0be302d4fcee7c44a9a8261df10e310fcac0
Sviatoslav <61809775+Simon-code1@users.noreply.github.com>
2024-02-18 12:28:06 -0500

    Email set up

--- Weld_Star_Corporation/settings.py
+++ Weld_Star_Corporation/settings.py
@@ -163,9 +163,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_HOST = 'mail.privateemail.com'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True 
+EMAIL_PORT = 465
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')                                            
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')    
 
